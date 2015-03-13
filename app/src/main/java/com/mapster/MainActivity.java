c01ac28a3d0ca48619372b7d021795850f7da3b9
Harriet Robinson-Chen <harrietrc@gmail.com>
2015-03-14 10:13:02 +1300

    Fiddling with markers and camera positioning
    Just following this tutorial:
    https://developers.google.com/maps/documentation/android/map#add_a_fragment

--- app/src/main/java/com/mapster/MainActivity.java
+++ app/src/main/java/com/mapster/MainActivity.java
@@ -1,20 +1,41 @@
 package com.mapster;
 
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends Activity {
+// FragmentActivity extends Activity, unsurprisingly
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* TESTING MAP FRAGMENTS */
+        // Don't worry, my actual code won't have this many comments...
+
+        // Nabbed from here https://developers.google.com/maps/documentation/android/map#add_map_code
+        // R.id.map is added automatically when the layout file is built
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        // Sets this as the callback object for when the GoogleMap instance is ready to use
+        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(-36.853085,174.76958))
+        .title("The University of Auckland"));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,4 +58,5 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
