de2d5bffe06c3a2911bc050cb073512ddccb1ea1
Harriet Robinson-Chen <harrietrc@gmail.com>
2015-03-14 11:01:35 +1300

    Sample listener that zooms/centres to fit all the markers

--- app/src/main/java/com/mapster/MainActivity.java
+++ app/src/main/java/com/mapster/MainActivity.java
@@ -7,34 +7,73 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // FragmentActivity extends Activity, unsurprisingly
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private List<Marker> _markers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* TESTING MAP FRAGMENTS */
-        // Don't worry, my actual code won't have this many comments...
-
-        // Nabbed from here https://developers.google.com/maps/documentation/android/map#add_map_code
         // R.id.map is added automatically when the layout file is built
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         // Sets this as the callback object for when the GoogleMap instance is ready to use
         mapFragment.getMapAsync(this);
+
+        _markers = new ArrayList<Marker>();
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(-36.853085,174.76958))
-        .title("The University of Auckland"));
+    public void onMapReady(final GoogleMap googleMap) {
+        newMarker(-36.853085, 174.76958, "The University of Auckland", googleMap);
+        newMarker(-36.848448,174.762191, "The Sky Tower", googleMap);
+        newMarker(-36.8273514, 174.811964, "North Head", googleMap);
+
+        googleMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                // Auto-zoom to fit all markers
+                // This builder creates a minimum bound based on a set of LatLng points
+                LatLngBounds.Builder b = new LatLngBounds.Builder();
+                for (Marker m: _markers) {
+                    b.include(m.getPosition());
+                }
+                LatLngBounds bounds = b.build();
+
+                // The second argument is padding
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+                googleMap.setOnCameraChangeListener(null);
+            }
+        });
+    }
+
+    /**
+     * Convenience method for adding new markers and keeping track of them in a list.
+     * @param latitude = Latitude value for marker
+     * @param longitude = Longitude value for marker
+     * @param name = Name associated with the marker
+     * @param map = Map that this marker will be added to
+     */
+    private void newMarker(double latitude, double longitude, String name, GoogleMap map) {
+        Marker marker = map.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude))
+                .title(name));
+        _markers.add(marker);
     }
 
     @Override
