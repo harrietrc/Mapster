6f9451f62ada999bc8381974d182abf42e9c1327
Tommy Ngo <ngo.tommy@outlook.com>
2015-03-14 15:33:59 +1300

    Make Mapster calculate routes for more than 2 POIs

---
+++ app/src/main/java/com/mapster/activities/MainActivity.java
@@ -0,0 +1,231 @@
+package com.mapster.activities;
+
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.mapster.R;
+import com.mapster.json.JSONParser;
+
+import org.apache.http.HttpConnection;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+// FragmentActivity extends Activity, unsurprisingly
+public class MainActivity extends FragmentActivity{
+
+//    private List<Marker> _markers;
+    private static final LatLng SKY_CITY = new LatLng(-37.044116, 175.0610719);
+    private static final LatLng CHRISTCHURCH = new LatLng(-43.5320544, 172.6362254);
+    private static final LatLng TAURANGA = new LatLng(-37.6877974, 176.1651295);
+    private static final LatLng ROTURUA = new LatLng(-38.1368478, 176.2497461);
+
+    GoogleMap googleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        googleMap = fm.getMap();
+
+        MarkerOptions options = new MarkerOptions();
+        options.position(SKY_CITY);
+        options.position(CHRISTCHURCH);
+        options.position(TAURANGA);
+        options.position(ROTURUA);
+        googleMap.addMarker(options);
+        String url = getMapsApiDirectionsUrl();
+        ReadTask downloadTask = new ReadTask();
+        downloadTask.execute(url);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SKY_CITY,
+                13));
+        addMarkers();
+
+//        // R.id.map is added automatically when the layout file is built
+//        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+//        // Sets this as the callback object for when the GoogleMap instance is ready to use
+//        mapFragment.getMapAsync(this);
+//
+//        _markers = new ArrayList<Marker>();
+
+    }
+
+    private String getMapsApiDirectionsUrl() {
+        String waypoints = "origin="
+                + SKY_CITY.latitude + "," + SKY_CITY.longitude
+                + "&waypoints=optimize:true|" + ROTURUA.latitude + "," + ROTURUA.longitude
+                + "|" + TAURANGA.latitude + "," + TAURANGA.longitude
+                + "&destination=" + CHRISTCHURCH.latitude + "," + CHRISTCHURCH.longitude ;
+        String output = "json";
+        String sensor = "&sensor=true";
+        String url = "https://maps.googleapis.com/maps/api/directions/"
+                + output + "?" + waypoints + sensor;
+        //url = "https://maps.googleapis.com/maps/api/directions/json?origin=41.3758887,2.1745799999999917&waypoints=41.39097711845494,2.1807326361331434|41.38680260504134,2.188132850805232|41.38458293055814,2.1758925899657697&destination=41.38394800519846,2.166872321048686&sensor=true&mode=walking";
+        return url;
+    }
+
+    private void addMarkers() {
+        if (googleMap != null) {
+            googleMap.addMarker(new MarkerOptions().position(SKY_CITY)
+                    .title("First Point"));
+            googleMap.addMarker(new MarkerOptions().position(CHRISTCHURCH)
+                    .title("Second Point"));
+            googleMap.addMarker(new MarkerOptions().position(TAURANGA).title("Third Point"));
+            googleMap.addMarker(new MarkerOptions().position(TAURANGA).title("Fourth Point"));
+        }
+    }
+
+    private class ReadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... url) {
+            String data = "";
+            try {
+                com.mapster.connectivities.HttpConnection http = new com.mapster.connectivities.HttpConnection();
+                data = http.readUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            new ParserTask().execute(result);
+        }
+    }
+
+    private class ParserTask extends
+            AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(
+                String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                JSONParser parser = new JSONParser();
+                routes = parser.parse(jObject);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> routes) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions polyLineOptions = null;
+            // traversing through routes
+            for (int i = 0; i < routes.size(); i++) {
+                points = new ArrayList<LatLng>();
+                polyLineOptions = new PolylineOptions();
+                List<HashMap<String, String>> path = routes.get(i);
+
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                polyLineOptions.addAll(points);
+                polyLineOptions.width(2);
+                polyLineOptions.color(Color.BLUE);
+            }
+
+            googleMap.addPolyline(polyLineOptions);
+        }
+    }
+
+//    @Override
+//    public void onMapReady(final GoogleMap googleMap) {
+//        newMarker(-36.853085, 174.76958, "The University of Auckland", googleMap);
+//        newMarker(-36.848448,174.762191, "The Sky Tower", googleMap);
+//        newMarker(-36.8273514, 174.811964, "North Head", googleMap);
+//
+//        googleMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+//            @Override
+//            public void onCameraChange(CameraPosition cameraPosition) {
+//                // Auto-zoom to fit all markers
+//                // This builder creates a minimum bound based on a set of LatLng points
+//                LatLngBounds.Builder b = new LatLngBounds.Builder();
+//                for (Marker m: _markers) {
+//                    b.include(m.getPosition());
+//                }
+//                LatLngBounds bounds = b.build();
+//
+//                // The second argument is padding
+//                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+//                googleMap.setOnCameraChangeListener(null);
+//            }
+//        });
+//
+//    }
+//
+//    /**
+//     * Convenience method for adding new markers and keeping track of them in a list.
+//     * @param latitude = Latitude value for marker
+//     * @param longitude = Longitude value for marker
+//     * @param name = Name associated with the marker
+//     * @param map = Map that this marker will be added to
+//     */
+//    private void newMarker(double latitude, double longitude, String name, GoogleMap map) {
+//        Marker marker = map.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude))
+//                .title(name));
+//        _markers.add(marker);
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
