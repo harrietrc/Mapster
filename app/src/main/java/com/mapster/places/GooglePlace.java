95917e81e1bd4c928e6678e7742c93f2da5dfad0
Harriet Robinson-Chen <harrietrc@gmail.com>
2015-03-17 07:03:20 +1300

    Sorted attractions into categories
    And changed filter categories. Some things like "electronics_store" and
    "gas_station" left out (should probably discuss what places are going to
    be included, but that's most of them)

--- app/src/main/java/com/mapster/places/GooglePlace.java
+++ app/src/main/java/com/mapster/places/GooglePlace.java
@@ -15,6 +15,33 @@ public class GooglePlace {
     public String longitude;
     public String name;
 
+    public static final String[] PLACES_OF_WORSHIP =  {
+        // Not really sure what to do with these - how do you differentiate something like the
+        // Sagrada Familia from just your local church? Probably by rating and popularity.
+        "church", "hindu_temple", "mosque", "place_of_worship", "synagogue"
+    };
+
+    public static final String[] ENTERTAINMENT = {
+        "bowling_alley", "casino", "movie_theatre", "night_club", "spa"
+    };
+
+    public static final String[] ATTRACTIONS = {
+        "amusement_park", "aquarium", "park", "zoo", "art_gallery", "museum",
+            "establishment" // Default when uncategorised
+    };
+
+    public static final String[] DINING = {
+        "bakery", "bar", "cafe", "food", "restaurant"
+    };
+
+    public static final String[] SHOPPING = {
+        "book_store", "clothing_store", "department_store", "shopping_mall"
+    };
+
+    public static final String[] ACCOMMODATION = {
+            "campground", "lodging" // Hotels, B&B's, etc. fall under lodging
+    };
+
     public GooglePlace() {
         // Private otherwise
     }
