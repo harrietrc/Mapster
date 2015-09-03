001e87a6958c8d1a5d0d49c3348428852bddbcc3
Tommy Ngo <ngo.tommy@outlook.com>
2015-09-03 14:37:11 +1200

    add icon and better UI

--- app/src/main/java/com/mapster/activities/MainActivity.java
+++ app/src/main/java/com/mapster/activities/MainActivity.java
@@ -900,13 +900,14 @@ public class MainActivity extends ActionBarActivity implements GoogleMap.OnMarke
             }
         });
 
-        _itineraryItem  = (ImageButton)ll.getChildAt(1);
+        _itineraryItem  = (ImageButton)ll.getChildAt(2);
         _itineraryItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startBudgetActivity();
             }
         });
+        ImageButton clear = (ImageButton)ll.getChildAt(1);
         MenuItem item = menu.findItem(R.id.action_toggle);
         if (_layout != null) {
             if (_layout.getPanelState() == SlidingUpPanelLayout.PanelState.HIDDEN) {
